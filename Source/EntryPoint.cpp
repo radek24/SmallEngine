a46e8f24b64dd7e6e7fc7edd0309d4de08cd7051
Radek <76497057+radek24@users.noreply.github.com>
2026-03-08 16:21:02 +0100

    Setup SDL3

--- Source/EntryPoint.cpp
+++ Source/EntryPoint.cpp
@@ -2,9 +2,30 @@
 
 #include "Log/Log.h"
 
-int main()
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+int main(int argc, char *argv[])
 {
     Log::Initialize();
+
     LOG_INFO("Test");
-    return 0;
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        LOG_ERROR("SDL_Init failed: {}", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SmallEngine", 800, 600, 0);
+    if (!window) {
+        LOG_ERROR("SDL_CreateWindow failed: {}", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    LOG_INFO("SDL initialized successfully");
+
+    SDL_Delay(10000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
